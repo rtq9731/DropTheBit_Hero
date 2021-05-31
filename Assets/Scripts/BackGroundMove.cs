82bd3dd32aefc433bedee7479dd4bb269f2f5c81
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-05-31 19:16:02 +0900

    배경 스크롤 구현

---
+++ Assets/Scripts/BackGroundMove.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackGroundMove : MonoBehaviour
+{
+    [SerializeField] List<Image> images;
+
+    [SerializeField] List<float> backgroundSpeeds;
+
+    List<float> vectorXList = new List<float>() { 0, 0, 0, 0};
+
+    private void Update()
+    {
+        for (int i = 0; i < vectorXList.Count; i++)
+        {
+            Mathf.Repeat(vectorXList[i], 10);
+        }
+
+        for (int i = 0; i < images.Count; i++)
+        {
+            vectorXList[i] += backgroundSpeeds[i] * Time.deltaTime;
+            images[i].material.mainTextureOffset = new Vector2(vectorXList[i], images[i].material.mainTextureOffset.y);
+        }
+    }
+}
